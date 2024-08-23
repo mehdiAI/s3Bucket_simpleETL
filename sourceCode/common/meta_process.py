2691ee3adbc26e318cc4675d69e68e5dd2844cd1
mehdiAI <fakourma12@gmail.com>
2024-08-23 22:45:39 +0330

    meta process initializing module

---
+++ sourceCode/common/meta_process.py
@@ -0,0 +1,16 @@
+"""
+    Methods for processing the meta file.
+"""
+
+class MetaProcess():
+    """class for working with the meta file.
+        metal file show tha whitch object and when inserted to the storage.
+        """
+    @staticmethod
+    def update_meta_file():
+        pass
+    
+    @staticmethod
+    def return_data_list():
+        pass
+    
