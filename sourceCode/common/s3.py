54f9710c93c07d2685342270949f3dea5a086501
mehdiAI <fakourma12@gmail.com>
2024-09-06 16:14:15 +0330

    clean the code s3.py by pylint hints

--- sourceCode/common/s3.py
+++ sourceCode/common/s3.py
@@ -1,44 +1,50 @@
 """Connector and methods accessing s3
 """
+
+import logging
+
 import boto3 # type: ignore
 
-import os
-import logging
+#import os
+
+
 
 
 class S3BucketConnector():
     """ 
         Class for interacting with S3 Buckets.
     """
-    
-    def __init__(self, access_key:str, secret_key:str, enfpoint_url:str, bucket:str) -> None:
+    def __init__(self, access_key:str, secret_key:str, endPoint_url:str, bucket:str) -> None:
         """
             Constructor for S3BucketConnector
             
             var1 access_key: access key for connecting to s3 bucket in the provider.
             var2 secret_key: secret key for connecting to s3 bucket in the provider.
-            var3 enfpoint_url: enfpoint_url for connecting to s3 bucket in the provider.
+            var3 endPoint_url: endPoint_url for connecting to s3 bucket in the provider.
             var4 bucket: bucket is a name of bucket for connecting to s3 in the provider.
-        """ 
-        self._logger = logging.getLogger(__name__) 
+        """
+        self._logger = logging.getLogger(__name__)
         self.access_key = access_key
         self.secret_key = secret_key
-        self.enfpoint_url = enfpoint_url
+        self.endPoint_url = endPoint_url
         self.bucket = bucket
         self.session = boto3.Session(aws_access_key_id=access_key,
                                      aws_secret_access_key=secret_key)
-        self._s3 = self.session.resource(service_name='s3',enfpoint_url=enfpoint_url)
+        self._s3 = self.session.resource(service_name='s3',endpoint_url=endPoint_url)
         self._bucket = self._s3.Bucket(bucket)
-    
+
     def preparing_s3Bucket_BucketObj_openDataset(self):
-        pass
-    
+        """_summary_
+        """
+
     def preparing_s3Bucket(self):
-        pass
-    
-    def assignDate_s3BucketObj(startDate,date_format,bucketObj_list):
+        """_summary_
+        """
+
+    def assignDate_s3BucketObj(self,startDate,date_format,bucketObj_list):
         """ covid19-harmonized-dataset in aws free dataset dont have date in files name then this 
-        function create a artificial date for each it object to use them to loading data section based on date.
+            function create a artificial date for each it object to use them to loading data section
+            based on date.
 
         Args:
             startDate (str): start of date to assigning to each Object.
@@ -48,8 +54,8 @@ class S3BucketConnector():
         Returns:
             pandas DataFrame: a table with date and s3 bucket objects columns.
         """
-        pass
-    
+
+
     def retrieveObjectFromS3Bucket(self):
         """
             Retrieve list all objects on the s3 bucket.
@@ -57,8 +63,8 @@ class S3BucketConnector():
             return: none and just print it in the output.
         """
         for obj in self._bucket.objects.all():
-            print(f"object_name: {obj.key} , last_modified: {obj.last_modified}") 
-        
+            print(f"object_name: {obj.key} , last_modified: {obj.last_modified}")
+
     def list_s3bucketObjects_in_prefix(self,key_prefix:str):
         """
             Retrieve list all objects with prefix filter on the s3 bucket.
@@ -68,15 +74,19 @@ class S3BucketConnector():
         """
         files = [obj.key for obj in self._bucket.objects.filter(Prefix=key_prefix)]
         return files
-    
+
     def read_csv_to_df(self):
-        pass
-    
+        """_summary_
+        """
+
     def read_parquet_to_df(self):
-        pass
-    
+        """_summary_
+        """
+
     def write_df_to_parquetObjS3(self):
-        pass
-    
+        """_summary_
+        """
+
     def write_df_to_csvObjS3(self):
-        pass
+        """_summary_
+        """
